e84a97897b7a8b14ddfd78a27cbb6c56446b41bc
Adam Paternostro <adpater@microsoft.com>
2020-04-13 12:24:09 -0400

    Deployed Function

--- Sample-End-Customer-Upload-To-Blob.sh
+++ Sample-End-Customer-Upload-To-Blob.sh
@@ -7,10 +7,12 @@
 # Example returned JSON
 # {"accountName":"landingzonestorage00005","containerName":"acmeinc","sasToken":"?sv=2018-03-28&sr=c&sig=uvhhtoqJhr8My5Oe73%2..."}
 
-azureFunctionUrl="http://localhost:7071"
+#azureFunctionUrl="http://localhost:7071"
+azureFunctionUrl="https://functionapp00005.azurewebsites.net"
 customerId="AcmeInc"
 customerSecret="0DC8B9026ECD402C84C66AFB5B87E28C"
-json=$(curl "$azureFunctionUrl/api/GetAzureStorageSASUploadToken?customerId=$customerId&customerSecret=$customerSecret")
+code="/dsLQephxBbVCNPI9kvWunO9s881t11XyCgI2xnh0a8PeRP4rsxJGw=="
+json=$(curl "$azureFunctionUrl/api/GetAzureStorageSASUploadToken?code=$code&customerId=$customerId&customerSecret=$customerSecret")
 
 # Parse the variables
 accountName="$(echo -n $json | jq .accountName --raw-output)"
@@ -23,10 +25,13 @@ echo "SAS Token:      $sasToken"
 
 today=`date +%Y-%m-%d`
 
+# Create test file
+echo "test" > myFile.txt
+
 # Upload 1 or more files to a folder with the date
-az storage blob upload --container-name $containerName --file ./myFile.txt --name /inbox/$today/myFile.txt --account-name $accountName --sas-token $sasToken    
+az storage blob upload --container-name $containerName --file ./myFile.txt --name inbox/$today/myFile.txt --account-name $accountName --sas-token $sasToken    
 
 # Single to Azure we are done by putting a marker complete file
 touch end_file_$today.txt
 
-az storage blob upload --container-name $containerName --file end_file_$today.txt --name /inbox/$today/end_file_$today.txt --account-name $accountName --sas-token $sasToken    
+az storage blob upload --container-name $containerName --file end_file_$today.txt --name inbox/$today/end_file_$today.txt --account-name $accountName --sas-token $sasToken    
