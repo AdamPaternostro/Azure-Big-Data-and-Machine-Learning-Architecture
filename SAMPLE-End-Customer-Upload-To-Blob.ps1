fb63cd6c88eef4eba63f69ed39940c35dc7907dc
Adam Paternostro <adpater@microsoft.com>
2020-04-15 17:07:57 -0400

    Updated upload samples

---
+++ SAMPLE-End-Customer-Upload-To-Blob.ps1
@@ -0,0 +1,84 @@
+###########################################################
+# This is a sample Powershell script an end customer can use to upload data to the landing zone
+# The customer will need to install PowerShell
+# Note: This does not work with the local storage emulator
+###########################################################
+
+
+###########################################################
+# Variables
+###########################################################
+$azureFunctionUrl="https://functionapp00005.azurewebsites.net"
+$customerId="AcmeInc"
+$customerSecret="0DC8B9026ECD402C84C66AFB5B87E28C"
+$azureFunctionCode="baBqKrKC97HA/sLvZvjHtxCq82a43UmevfNSOwJU9DSuUXt6dUAixA=="
+$today=(Get-Date).ToString('yyyy-MM-dd')
+
+
+###########################################################
+# Call to get SAS token
+###########################################################
+$json=(Invoke-restmethod -Uri "$azureFunctionUrl/api/GetAzureStorageSASUploadToken?code=$azureFunctionCode&customerId=$customerId&customerSecret=$customerSecret")
+
+
+###########################################################
+# Parse the returned values
+###########################################################
+$accountName=$json.accountName
+$containerName=$json.containerName
+$sasToken=$json.sasToken
+
+Write-Output "Account Name:   $accountName"
+Write-Output "Container Name: $containerName"
+Write-Output "SAS Token:      $sasToken"
+
+
+###########################################################
+# Create a Test Sample file to upload
+###########################################################
+Write-Output "CustomerId,CustomerName" > myFile.txt
+Write-Output "1,Microsoft" >> myFile.txt
+Write-Output "2,Contoso"   >> myFile.txt
+Write-Output "3,Acme"      >> myFile.txt
+Write-Output "4,Walmart"   >> myFile.txt
+Write-Output "5,Target"    >> myFile.txt
+
+
+###########################################################
+# Upload one or many files
+#
+# NOTES: 
+# 1. You could use Azure PowerShell
+# 2. You could use Azure CLI
+# 3. You could use Azure azcopy commands
+###########################################################
+
+# Target path (replace myFile.txt with your file name)
+$uri = "https://$accountName.blob.core.windows.net/$containerName/inbox/$today/myFile.txt$sasToken"
+
+$headers = @{
+    'x-ms-blob-type' = 'BlockBlob'
+}
+
+# Upload file using just REST
+Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile myFile.txt
+
+
+###########################################################
+# Upload the signal file that we are done uploading
+# Single to Azure we are done by putting a marker complete file
+# The file MUST end in "end_file.txt"
+###########################################################
+
+# Create a marker file
+Write-Output "" > end_file.txt
+
+# Target path
+$uri = "https://$accountName.blob.core.windows.net/$containerName/inbox/$today/end_file.txt$sasToken"
+
+$headers = @{
+    'x-ms-blob-type' = 'BlockBlob'
+}
+
+# Upload file using just REST
+Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile myFile.txt
--- Sample-End-Customer-Upload-To-Blob.sh
+++ Sample-End-Customer-Upload-To-Blob.sh
@@ -1,20 +1,29 @@
-# NOTES:
-# This is a sample bash script an end customer can use to upload data to the landing zone
+###########################################################
+# This is a sample Bash script an end customer can use to upload data to the landing zone
 # The customer will need to install "jq" to parse the returned JSON: https://stedolan.github.io/jq/download/
-
 # Note: This does not work with the local storage emulator
+###########################################################
 
-# Example returned JSON
-# {"accountName":"datalake00005","containerName":"acmeinc","sasToken":"?sv=2018-03-28&sr=c&sig=GizH0qjGsshuQHq9sIYyw5V%2Bkpwh1lSH7D7G4qPHAxk%3D&st=2020-04-15T14%3A41%3A26Z&se=2020-04-15T15%3A46%3A26Z&sp=wl"}
 
-#azureFunctionUrl="http://localhost:7071"
+###########################################################
+# Variables
+###########################################################
 azureFunctionUrl="https://functionapp00005.azurewebsites.net"
 customerId="AcmeInc"
 customerSecret="0DC8B9026ECD402C84C66AFB5B87E28C"
-code="baBqKrKC97HA/sLvZvjHtxCq82a43UmevfNSOwJU9DSuUXt6dUAixA=="
-json=$(curl "$azureFunctionUrl/api/GetAzureStorageSASUploadToken?code=$code&customerId=$customerId&customerSecret=$customerSecret")
+azureFunctionazureFunctionCode="baBqKrKC97HA/sLvZvjHtxCq82a43UmevfNSOwJU9DSuUXt6dUAixA=="
+today=`date +%Y-%m-%d`
 
-# Parse the variables
+
+###########################################################
+# Call to get SAS token
+###########################################################
+json=$(curl "$azureFunctionUrl/api/GetAzureStorageSASUploadToken?code=$azureFunctionazureFunctionCode&customerId=$customerId&customerSecret=$customerSecret")
+
+
+###########################################################
+# Parse the returned values
+###########################################################
 accountName="$(echo -n $json | jq .accountName --raw-output)"
 containerName="$(echo -n $json | jq .containerName --raw-output)"
 sasToken="$(echo -n $json | jq .sasToken --raw-output)"
@@ -23,9 +32,11 @@ echo "Account Name:   $accountName"
 echo "Container Name: $containerName"
 echo "SAS Token:      $sasToken"
 
-today=`date +%Y-%m-%d`
 
-# Create test file
+
+###########################################################
+# Create a Test Sample file to upload
+###########################################################
 echo "CustomerId,CustomerName" > myFile.txt
 echo "1,Microsoft" >> myFile.txt
 echo "2,Contoso"   >> myFile.txt
@@ -34,13 +45,24 @@ echo "4,Walmart"   >> myFile.txt
 echo "5,Target"    >> myFile.txt
 
 
-# Upload 1 or more files to a folder with the date
-# NOTE: You can also use azcopy
-# ./azcopy copy ./myFolder "https://$landingzonestorage00005.blob.core.windows.net/$containerName$sasToken" --recursive
+###########################################################
+# Upload one or many files
+#
+# NOTES: 
+# 1. You could use Azure PowerShell (Core)
+# 2. You could use Azure CLI
+# 3. You could use Azure azcopy commands
+###########################################################
 az storage blob upload --container-name $containerName --file ./myFile.txt --name inbox/$today/myFile.txt --account-name $accountName --sas-token $sasToken    
 
+
+###########################################################
+# Upload the signal file that we are done uploading
 # Single to Azure we are done by putting a marker complete file
 # The file MUST end in "end_file.txt"
+###########################################################
+
+# Create a marker file
 touch end_file.txt
 
 az storage blob upload --container-name $containerName --file end_file.txt --name inbox/$today/end_file.txt --account-name $accountName --sas-token $sasToken    
